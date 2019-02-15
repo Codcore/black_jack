0f8092e316549c7207b62f577ebe9b00ee9e1bba
Andrew Mark <codcore@gmail.com>
2019-02-15 17:21:20 +0200

    Improve: Hand#full?, #pull_card: change '.length' to '.size'

--- hand.rb
+++ hand.rb
@@ -2,7 +2,7 @@ class Hand
   attr_reader :cards
 
   def initialize(*args)
-    raise "Hand should be initialized with 2 cards, not #{args.length}." if args.length > 2
+    raise "Hand should be initialized with 2 cards, not #{args.size}." if args.size > 2
 
     @cards = []
     @cards.concat(args)
@@ -15,6 +15,6 @@ class Hand
   end
 
   def full?
-    true if @cards.length == 3
+    true if @cards.size == 3
   end
 end
