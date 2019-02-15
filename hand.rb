3d56484361eb52b50aec79a1cdc0dafda5dc0971
Andrew Mark <codcore@gmail.com>
2019-02-15 17:18:50 +0200

    Improve: Hand#initialize, #pull_card: raise exeptions

--- hand.rb
+++ hand.rb
@@ -2,11 +2,15 @@ class Hand
   attr_reader :cards
 
   def initialize(*args)
+    raise "Hand should be initialized with 2 cards, not #{args.length}." if args.length > 2
+
     @cards = []
     @cards.concat(args)
   end
 
   def pull_card(card)
+    raise 'Cannot pull cards no more: hand is full.' if full?
+
     @cards << card
   end
 
