59b6806b1fa71fd6bd66e01e841853ee85e136b5
Andrew Mark <codcore@gmail.com>
2019-02-16 12:53:26 +0200

    Add: Interface#show_hands, Interface#show_middle_limiter

--- interface.rb
+++ interface.rb
@@ -5,7 +5,7 @@ class Interface
 
   def show_msg(msg, start_limiter = true, end_limiter = true)
     show_start_limiter if start_limiter
-    str_length = 42
+    str_length = 62
     to_fill    = str_length - msg.length
     fill_start = to_fill / 2
     fill_end   = to_fill - fill_start
@@ -20,11 +20,15 @@ class Interface
   end
 
   def show_prompt(msg)
-    puts "⟹#{msg}:"
+    puts "⟹   #{msg}:"
   end
 
   def show_start_limiter
     puts LIMITER_START
+  end
+
+  def show_middle_limiter
+    puts LIMITER_MIDDLE
   end
 
   def show_end_limiter
@@ -43,12 +47,20 @@ class Interface
     show_msg(construct_menu(array))
   end
 
+  def show_hands(player_name, player_cards, dealer_cards)
+    show_msg('Dealer cards:', true, false)
+    show_msg(dealer_cards, false, false)
+    show_middle_limiter
+    show_msg("#{player_name}, Your cards:", false, false)
+    show_msg(player_cards, false, true)
+  end
+
   private
 
   def construct_menu(array)
     menu_str = ''
     array.each.with_index(1) do |item, index|
-      menu_str << "[#{index} #{item}] "
+      menu_str << "[(#{index}) - #{item}]  "
     end
     menu_str
   end
