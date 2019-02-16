2738957c0fecb2c7a67d119a475c112f6b6fbc22
Andrew Mark <codcore@gmail.com>
2019-02-16 16:58:19 +0200

    Add: actions methods for GameController

--- game_controller.rb
+++ game_controller.rb
@@ -16,13 +16,17 @@ class GameController
 
   def run
     loop do
-      @interface.show_game_screen(@game.dealer, @game.user)
+      @interface.show_game_screen(@dealer, @user)
       @interface.show_actions
       choice = @interface.user_choice(ACTIONS_MENU.size)
       @interface.show_msg(INVALID_CHOICE) unless choice
       case choice
       when 1 then action_skip
+      when 2 then action_pull_card
+      when 3 then action_open_cards
+      else next
       end
+      game_over(determine_winner) if @user.hand.full? && @dealer.hand.full?
     end
   end
 
@@ -31,10 +35,12 @@ class GameController
     @interface.show_menu(START_MENU)
     choice = @interface.user_choice(START_MENU.size)
     case choice
-    when 2 then return
     when 1
       @interface.show_prompt(USER_NAME_PROMPT)
-      @game = Game.new(@interface.user_input)
+      @game   = Game.new(@interface.user_input)
+      @user   = @game.user
+      @dealer = @game.dealer
+    when 2 then return
     else
       @interface.show_msg(INVALID_CHOICE)
       setup
@@ -42,6 +48,39 @@ class GameController
   end
 
   def action_skip
-    @game.dealer.move
+    @dealer.move
+    game_over(@user) if @dealer.points > 21
+  end
+
+  def action_pull_card
+    @user.hand.pull_card(CardDeck.instance.draw_card)
+    game_over(@dealer) if @user.points > 21
+  end
+
+  def action_open_cards
+    @dealer.hand.visible = true
+    game_over(determine_winner)
+  end
+
+  def determine_winner
+    return nil if @dealer.points == @user.points
+
+    #TODO: BLACK_JACK module
+    21 - @user.points < 21 - @dealer.points ? @user : @dealer
+  end
+
+  def game_over(winner)
+    @dealer
+    @interface.show_msg("#{winner.name.upcase} IS WIN!!!")
+    #TODO: merge to one method
+    @interface.show_menu(GAME_OVER_MENU)
+    choice = @interface.user_choice(GAME_OVER_MENU.size)
+    case choice
+    when 1
+      CardDeck.instance.refresh
+      return
+    when 2
+      exit
+    end
   end
 end
