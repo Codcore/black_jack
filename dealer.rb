b2fca3df7aef3ca0ed00b0f76e9d0ed68c2b981a
Andrew Mark <codcore@gmail.com>
2019-02-16 15:58:36 +0200

    Add: base Dealer#move

--- dealer.rb
+++ dealer.rb
@@ -4,4 +4,10 @@ class Dealer < Player
   def initialize
     super('Dealer', false)
   end
+
+  def move
+    return if points >= 17
+
+    @hand.pull_card(CardDeck.instance.draw_card) unless @hand.full?
+  end
 end
